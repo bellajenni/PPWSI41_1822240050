0a02b9a88493184004cc112ff9f1eeaa029c2f95
Bella Jenni Ourelia <bellajenni@mhs.mdp.ac.id>
2021-06-13 20:10:35 +0700

    Membuat CRUD API Resource

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\MahasiswaApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,29 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/", function(){
+    return array("status" => true, "pesan" => "Hallo Sayang");
+});
+
+Route::get("/hallo/{nama?}/{npm?}", function($nama = "Joe", $npm = NULL){
+    $mhs = array(
+        "nama" => $nama,
+        "npm" => $npm);
+    return array(
+        "status" => true, 
+        "pesan" => "Hallo $nama",
+        "mhs" => $mhs
+    );
+});
+// Buat Controller > php artisan make:controller HomeController
+Route::get("/home", [HomeController::class,  "index"]);
+Route::get("/home/kenalan/{nama?}", [HomeController::class,  "kenalan"]);
+
+//Buat API Controller atau Resource Controller utk kelola data MHS
+//Nama Controller adalah MahasiswaController
+// php artisan make:controller MahasiswaController --api
+//Daftar Resource Controller :MahasiswaController ke path mahasiswa
+Route::apiResource("mahasiswa", MahasiswaController::class);
+Route::get('/mahasiswa/insert', 'MahasiswaController@insert');
+Route::apiResource("mhs", MahasiswaApiController::class);
